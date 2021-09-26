87b12ca9a3b63a910a33efc5099666be5b56c0ef
bhrugup <40659156+bhrugup@users.noreply.github.com>
2021-09-26 12:43:25 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@ $message = $_POST['message'];
 $formcontent=" From: $name \n Subject: $subject \n Message: $message";
 $recipient = "bm.pandya2710@gmail.com";
 $subject = "Contact Form";
-$mailheader = "From: .$email. \r\n";
+$mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!" . " -" . "<a href='index.html' > Return Home</a>";
 ?>
